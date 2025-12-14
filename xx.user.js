8340e5368df224b61dfcd6f6cd3afd23a923c57b
Maximilian <maximilian.nink@web.de>
2025-12-14 11:31:26 +0100

    Create xx.user.js

---
+++ xx.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         xxx
+// @namespace    http://tampermonkey.net/
+// @version      2.0.2
+// @description  xxx
+// @author       Max8
+// @match        https://www.leitstellenspiel.de/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=leitstellenspiel.de
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+})();
