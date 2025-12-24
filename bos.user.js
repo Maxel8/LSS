ce41c56273cf31705e343c6e5179b04fd6638cbf
Maximilian <maximilian.nink@web.de>
2025-12-24 12:55:02 +0100

    Update bos.user.js

--- bos.user.js
+++ bos.user.js
@@ -1,46 +1,55 @@
 // ==UserScript==
-// @name         BOS-Fahrzeuge Kreisgrenzen (Google Maps)
+// @name         BOS-Fahrzeuge – Kreisgrenzen Overlay
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0
-// @description  Zeigt deutsche Kreisgrenzen auf der Google-Map von bos-fahrzeuge.de
-// @author       Max8
-// @match        https://www.bos-fahrzeuge.de/*
+// @version      0.2.0
+// @match        https://www.bos-fahrzeuge.info/*
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  'use strict';
+    'use strict';
 
+    let capturedMap = null;
 
-  const GEOJSON_URL = 'https://raw.githubusercontent.com/jalibu/LSHeat/master/kreise.json';
+    const waitForGoogle = setInterval(() => {
+        if (window.google && google.maps && google.maps.Map) {
+            clearInterval(waitForGoogle);
 
-  function waitForMap(cb) {
-    const i = setInterval(() => {
-      if (window.google && window.google.maps && window.map) {
-        clearInterval(i);
-        cb(window.map);
-      }
-    }, 500);
-  }
+            const OriginalMap = google.maps.Map;
 
-  waitForMap((map) => {
-    fetch(GEOJSON_URL)
-      .then(r => r.json())
-      .then(geojson => {
-        geojson.features.forEach(f => {
-          const paths = f.geometry.coordinates.map(poly =>
-            poly[0].map(c => ({ lat: c[1], lng: c[0] }))
-          );
+            google.maps.Map = function (...args) {
+                const map = new OriginalMap(...args);
+                capturedMap = map;
 
-          new google.maps.Polygon({
-            paths: paths,
-            strokeColor: '#ff0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillOpacity: 0,
-            map: map
-          });
-        });
-      });
-  });
+                // leicht verzögert, damit die Seite fertig initialisiert ist
+                setTimeout(() => drawKreisgrenzen(map), 1500);
+
+                return map;
+            };
+        }
+    }, 200);
+
+    function drawKreisgrenzen(map) {
+        fetch('https://raw.githubusercontent.com/jalibu/LSHeat/master/kreise.json')
+            .then(r => r.json())
+            .then(geo => {
+                geo.features.forEach(f => {
+                    f.geometry.coordinates.forEach(poly => {
+                        const path = poly[0].map(c => ({
+                            lat: c[1],
+                            lng: c[0]
+                        }));
+
+                        new google.maps.Polygon({
+                            paths: path,
+                            strokeColor: '#cc0000',
+                            strokeOpacity: 0.7,
+                            strokeWeight: 1,
+                            fillOpacity: 0,
+                            map: map
+                        });
+                    });
+                });
+            });
+    }
 })();
