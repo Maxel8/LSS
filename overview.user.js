17438afd41048845078f221036a208a07f1403e3
Maximilian <maximilian.nink@web.de>
2026-03-23 14:22:13 +0100

    Add user script metadata for LSS Overview

--- overview.user.js
+++ overview.user.js
@@ -1,1 +1,16 @@
+// ==UserScript==
+// @name         [LSS] Overview
+// @namespace    http://tampermonkey.net/
+// @version      v0.1
+// @description  Dashboard for LSS
+// @author       Max8
+// @match        https://www.leitstellenspiel.de/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=leitstellenspiel.de
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
